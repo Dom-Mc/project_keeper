0a6067e55b058f12f8fd5394e28a09bec93ac37b
Dom McKellar <dom.mckellar@gmail.com>
2016-08-01 09:50:33 -0700

    Add environment.rb, setup bundler and db connection

---
+++ config/environment.rb
@@ -0,0 +1,16 @@
+# environment.rb file: will connect files in our app with each other and to our various gems (via bundler)
+
+# Setup environment
+ENV['SINATRA_ENV'] ||= "development"
+
+# Setup bundler
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
+# Setup database connection
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+)
+
+require_all 'app'
