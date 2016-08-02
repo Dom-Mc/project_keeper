635fbee678cc5a21dc2f125b9e39bd0a2de48116
Dom McKellar <dom.mckellar@gmail.com>
2016-08-02 16:01:10 -0700

    Update exsisting and add additional columns on projects table

--- db/migrate/20160801210343_create_projects.rb
+++ db/migrate/20160801210343_create_projects.rb
@@ -2,11 +2,17 @@ class CreateProjects < ActiveRecord::Migration
 
   def change
     create_table :projects do |t|
-      t.string :name
-      t.string :description
+      t.string :name, null: false
+      t.text :description
+      t.string :date_started, null: false
       t.string :date_completed
-      t.integer :user_id
+      t.string :deadline
+      t.text :notes
+      t.boolean :status, null: false, default: false
+      t.references :user
+      t.timestamps
     end
+    add_index :projects, :name
   end
 
 end
