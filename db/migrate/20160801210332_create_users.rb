06cd5d4754c534d9f6ab74d6cb870f7f77fb550f
Dom McKellar <dom.mckellar@gmail.com>
2016-08-02 16:06:23 -0700

    Update exsisting and add additional columns to users table

--- db/migrate/20160801210332_create_users.rb
+++ db/migrate/20160801210332_create_users.rb
@@ -2,10 +2,14 @@ class CreateUsers < ActiveRecord::Migration
 
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :first_name, null: false
+      t.string :last_name, null: false
       t.string :email
       t.string :password_digest
+      t.string :username, null: false
+      t.timestamps
     end
+    add_index :users, :username
   end
 
 end
