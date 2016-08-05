c944a17d189d14dcb9521deba547e9acbd014616
Dom McKellar <dom.mckellar@gmail.com>
2016-08-05 13:12:38 -0700

    Add private methods as helpers to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,34 @@ class ApplicationController < Sinatra::Base
     "Hello World!!!!"
   end
 
+
+  private
+
+    def log_in(user)
+      session[:user_id] = user.id
+    end
+
+    def current_user
+      if (user_id = session[:user_id])
+        #prevents multiple calls to the database
+        @current_user ||= User.find_by(id: session[:user_id])
+      end
+    end
+
+    def verify_correct_user
+      @user = User.find_by(username: params[:username])
+      redirect "/#{@current_user.username}/projects" unless @user == current_user
+    end
+
+    def logged_in?
+      !current_user.nil? #!!session[:user_id]
+    end
+
+    def verify_logged_in
+      unless logged_in?
+        # TODO: flash message (please login)
+        redirect '/login'
+      end
+    end
+
 end
