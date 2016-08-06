47fa86256c25e6da386023d6c45ffee83fe9bdee
Dom McKellar <dom.mckellar@gmail.com>
2016-08-05 20:49:08 -0700

    Add methods to convert project names to url friendly slugs

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,14 @@
 class Project < ActiveRecord::Base
   belongs_to :user
+  
+  def slug
+    self.name.downcase.gsub(/\s+/, '-')
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find do |user|
+      user.slug == slug
+    end
+  end
+
 end
