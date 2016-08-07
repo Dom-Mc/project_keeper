d5cb6749f8b83ab828932c5cac35cdddc0244ae7
Dom McKellar <dom.mckellar@gmail.com>
2016-08-06 20:13:26 -0700

    Add activerecord validations to Project and User models

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,23 @@
 class Project < ActiveRecord::Base
+
   belongs_to :user
-  
+  DATE_REGEX = /\d{1,2}[-]\d{1,2}[-]\d{2,4}/i
+
+  validates :date_completed, :deadline, format: { with:  DATE_REGEX },
+                                        allow_nil: true
+
+  validates :description, :notes, length: { maximum: 255 },
+                                  allow_nil: true
+
+  validates :name, presence: true,
+                   length: { maximum: 50 }
+
+  validates :date_started, presence: true,
+                           format: { with:  DATE_REGEX }
+
+  validates :status, presence: true,
+                     inclusion: { in: ["In Progress", "On Hold", "Completed"] }
+
   def slug
     self.name.downcase.gsub(/\s+/, '-')
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,28 @@
 class User < ActiveRecord::Base
+
   has_many :projects
   has_secure_password
+  EMAIL_REGEX = /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
+
+  validates :first_name, :last_name, presence: true,
+                                     length: { maximum: 30 }
+
+  validates :email, length: { maximum: 200 },
+                    format: { with: EMAIL_REGEX },
+                    allow_nil: true
+
+  validates :username, presence: true,
+                       uniqueness: true,
+                       length: { in: 3..25 },
+                       exclusion: { in: %w(admin administrator root),
+                         message: "%{value} is reserved." }
+
+  validates :password, length: { in: 6..15 },
+                       presence: true,
+                       on: :save
+
+  validates :password, length: { in: 6..15 },
+                       allow_nil: true,
+                       on: :update
+
 end
