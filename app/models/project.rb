985171305c4d409a75c8345d9813417c2eb9268f
Dom McKellar <dom.mckellar@gmail.com>
2016-08-09 08:37:48 -0700

    Refactor validate methods and DATE_REGEX expression

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,22 +1,22 @@
 class Project < ActiveRecord::Base
 
   belongs_to :user
-  DATE_REGEX = /\d{1,2}[-]\d{1,2}[-]\d{2,4}/i
-
-  validates :date_completed, :deadline, format: { with:  DATE_REGEX },
-                                        allow_nil: true
-
-  validates :description, :notes, length: { maximum: 255 },
-                                  allow_nil: true
+  DATE_REGEX = /\d{4}[-]\d{2}[-]\d{2}/
 
   validates :name, presence: true,
-                   length: { maximum: 50 }
+                   length: { maximum: 50 },
+                   uniqueness: { scope: :user_id,
+                     message: "Please choose a different project name, '{value}' is already in use." }
 
-  validates :date_started, presence: true,
-                           format: { with:  DATE_REGEX }
+ validates :status, presence: true,
+                    inclusion: { in: ["In Progress", "On Hold", "Completed"] }
 
-  validates :status, presence: true,
-                     inclusion: { in: ["In Progress", "On Hold", "Completed"] }
+  validates :date_started, :date_completed, :deadline, format: { with:  DATE_REGEX,
+                                              message: 'must be a valid date.',
+                                              allow_blank: true }
+
+  validates :description, :notes, length: { maximum: 255 },
+                                    allow_blank: true
 
   def slug
     self.name.downcase.gsub(/\s+/, '-')
