45d00cc7db5301cb025cda3ec738ffc139e8cf4d
Dom McKellar <dom.mckellar@gmail.com>
2016-08-12 15:58:41 -0700

    Replace route validations with new helpers, add flash messages

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,64 +1,56 @@
 class ProjectsController < ApplicationController
 
-  get '/:username/projects' do
-    verify_logged_in
-    verify_correct_user
+  get '/:username/projects/?' do
+    user_authenticated?
     @projects = @user.projects
     erb :'projects/index'
   end
 
   get '/:username/projects/new' do
-    verify_logged_in
-    verify_correct_user
+    user_authenticated?
     @project = Project.new
     erb :'projects/new'
   end
 
   post '/:username/projects' do
-    verify_logged_in
-    verify_correct_user
+    user_authenticated?
     @project = Project.new(params[:project])
     if @project.save
       @user.projects << @project
+      flash[:success] = "Your new project was successfully created!"
       redirect "/#{@user.username}/projects/#{@project.slug}"
     else
-      # TODO: Add flash message (list errors)
       erb :'projects/new'
     end
   end
 
   get '/:username/projects/:slug' do
-    verify_logged_in
-    verify_correct_user
-    @project = Project.find_by_slug(params[:slug])
+    user_authenticated?
+    slugify(params[:slug])
     erb :'projects/show'
   end
 
   get '/:username/projects/:slug/edit' do
-    verify_logged_in
-    verify_correct_user
-    @project = Project.find_by_slug(params[:slug])
+    user_authenticated?
+    slugify(params[:slug])
     erb :'projects/edit'
   end
 
   patch '/:username/projects/:slug' do
-    verify_logged_in
-    verify_correct_user
-    @project = Project.find_by_slug(params[:slug])
+    user_authenticated?
+    slugify(params[:slug])
     if @project.update(params[:project])
-      # TODO: add flash message (successfully updated)
+      flash[:success] = "Your project was successfully updated!"
       redirect "/#{@user.username}/projects/#{@project.slug}"
     else
-      # TODO: add flash message (list errors)
-      erb :'users/edit'
+      erb :'projects/edit'
     end
   end
 
   delete '/:username/projects/:slug/delete' do
-    verify_logged_in
-    verify_correct_user
-    Project.find_by_slug(params[:slug]).destroy
-    # TODO: add flash message (successfully deleted)
+    user_authenticated?
+    slugify(params[:slug]).delete
+    flash[:success] = "Your project was successfully deleted."
     redirect to "/#{@user.username}/projects"
   end
 
